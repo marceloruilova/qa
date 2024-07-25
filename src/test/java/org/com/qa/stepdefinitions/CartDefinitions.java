3af69645cd9b162d69c2dea30f72f54ef8db7315
marcelo@touwolf.com <marcelo@touwolf.com>
2024-07-24 22:26:06 -0500

    steps

--- src/test/java/org/com/qa/stepdefinitions/CartDefinitions.java
+++ src/test/java/org/com/qa/stepdefinitions/CartDefinitions.java
@@ -3,19 +3,19 @@ package org.com.qa.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.com.qa.tasks.HomeScreenplayTest;
 
 public class CartDefinitions {
 
+    HomeScreenplayTest main = new HomeScreenplayTest();
+
     @Given("a list of products on the main page")
     public void aListOfProductsOnTheMainPage() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
 
     @When("the user clicks a product")
     public void theUserClicksAProduct() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        main.testAddToCart();
     }
 
     @Then("product should be added to the cart")
